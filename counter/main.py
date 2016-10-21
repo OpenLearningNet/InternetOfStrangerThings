d0d886b875f3dad5a28eac195adff5e7889a56a5
David Collien <david@openlearning.com>
2016-10-21 17:19:48 +1100

    init wifi connect

--- counter/main.py
+++ counter/main.py
@@ -1,1 +1,14 @@
-# TODO
+def wifi_connect(essid, password):
+  wlan = network.WLAN(network.STA_IF)
+  wlan.active(True)
+  if not wlan.isconnected():
+    print('connecting to network...')
+    wlan.connect(essid, password)
+    while not wlan.isconnected():
+      time.sleep_ms(100)
+      print('.', end='')
+  print(' connected.')
+  print('network config:', wlan.ifconfig())
+  return wlan
+
+wifi_connect('openlearning.com', 'AbsurdCyclicDungeonPipe')
